62ddeab393ae850a110d70787992a5dbd27d601a
Shin Young Jei <zerojei0423@naver.com>
2022-09-14 16:58:54 +0900

    feat: 로그인 앱 추가

--- frontend/mountaindo/App.tsx
+++ frontend/mountaindo/App.tsx
@@ -3,12 +3,19 @@ import {NavigationContainer} from '@react-navigation/native';
 import {Provider} from 'react-redux';
 import store from './src/store';
 import AppInner from './AppInner';
+import {useState} from 'react';
+import SignIn from './src/pages/SignIn';
+
+export type RootStackParamList = {
+  SignIn: any;
+};
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <Provider store={store}>
       <NavigationContainer>
-        <AppInner />
+        {isLoggedIn ? <AppInner /> : <SignIn />}
       </NavigationContainer>
     </Provider>
   );
