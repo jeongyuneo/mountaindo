7f5cc9ea951b1872ea0453d7ef4c69861c669158
ska05142 <ska05142@naver.com>
2022-09-14 17:22:08 +0900

    feat: 회원 정보 찾기 페이지 구현

--- frontend/mountaindo/src/pages/SignIn.tsx
+++ frontend/mountaindo/src/pages/SignIn.tsx
@@ -1,9 +1,26 @@
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import React from 'react';
-import {View, Text} from 'react-native';
-function SignIn() {
+import {View, Text, Pressable} from 'react-native';
+import {RootStackParamList} from '../../AppInner';
+
+type SignInScreenProps = NativeStackScreenProps<RootStackParamList, 'SignIn'>;
+
+function SignIn({navigation}: SignInScreenProps) {
   return (
     <View>
       <Text>로그인</Text>
+      <Pressable
+        onPress={() => {
+          navigation.navigate('FindId');
+        }}>
+        <Text>아이디 찾기</Text>
+      </Pressable>
+      <Pressable
+        onPress={() => {
+          navigation.navigate('FindPassword');
+        }}>
+        <Text>비밀번호 찾기</Text>
+      </Pressable>
     </View>
   );
 }
