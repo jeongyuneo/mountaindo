f774e619fb895a5c7eae8ae4df9779187c79004f
jeonyuneo <piq2255@gmail.com>
2022-09-15 23:26:53 +0900

    feat: HikingService 추가

---
+++ backend/src/main/java/com/hanssarang/backend/hiking/service/HikingService.java
@@ -0,0 +1,12 @@
+package com.hanssarang.backend.hiking.service;
+
+import com.hanssarang.backend.hiking.controller.dto.HikingListResponse;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HikingService {
+
+    public HikingListResponse getHikings(int memberId) {
+        return null;
+    }
+}
