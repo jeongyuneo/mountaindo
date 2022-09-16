b08ac857a856ac8fa27657b78acaecef8dd13cc6
jeonyuneo <piq2255@gmail.com>
2022-09-16 15:31:42 +0900

    feat: HikingService - 등산상세 조회 추가

--- backend/src/main/java/com/hanssarang/backend/hiking/service/HikingService.java
+++ backend/src/main/java/com/hanssarang/backend/hiking/service/HikingService.java
@@ -1,12 +1,17 @@
 package com.hanssarang.backend.hiking.service;
 
 import com.hanssarang.backend.hiking.controller.dto.HikingListResponse;
+import com.hanssarang.backend.hiking.controller.dto.HikingResponse;
 import org.springframework.stereotype.Service;
 
 @Service
 public class HikingService {
 
     public HikingListResponse getHikings(int memberId) {
+        return null;
+    }
+
+    public HikingResponse getHiking(int hikingId) {
         return null;
     }
 }
