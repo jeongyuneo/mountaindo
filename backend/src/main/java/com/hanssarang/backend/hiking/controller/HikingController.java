4966d34bcaa599d51acfbe8d385de6268df911ad
jeonyuneo <piq2255@gmail.com>
2022-09-16 15:31:56 +0900

    feat: HikingController - 등산상세 조회 추가

--- backend/src/main/java/com/hanssarang/backend/hiking/controller/HikingController.java
+++ backend/src/main/java/com/hanssarang/backend/hiking/controller/HikingController.java
@@ -1,13 +1,11 @@
 package com.hanssarang.backend.hiking.controller;
 
 import com.hanssarang.backend.hiking.controller.dto.HikingListResponse;
+import com.hanssarang.backend.hiking.controller.dto.HikingResponse;
 import com.hanssarang.backend.hiking.service.HikingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -19,5 +17,10 @@ public class HikingController {
     @GetMapping
     public ResponseEntity<HikingListResponse> getHikings(@RequestHeader("Authorization") String token) {
         return ResponseEntity.ok(hikingService.getHikings(1));
+    }
+
+    @GetMapping("/{hikingId}")
+    public ResponseEntity<HikingResponse> getHiking(@RequestHeader("Authorization") String token, @PathVariable int hikingId) {
+        return ResponseEntity.ok(hikingService.getHiking(1));
     }
 }
