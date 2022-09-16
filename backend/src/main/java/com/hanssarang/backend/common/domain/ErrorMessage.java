6de43af3ab4d9b7c26075ec86136e82241030e1e
jeonyuneo <piq2255@gmail.com>
2022-09-16 15:43:53 +0900

    feat: ErrorMessage - 등산 정보 조회 불가 메시지 추가

--- backend/src/main/java/com/hanssarang/backend/common/domain/ErrorMessage.java
+++ backend/src/main/java/com/hanssarang/backend/common/domain/ErrorMessage.java
@@ -3,4 +3,5 @@ package com.hanssarang.backend.common.domain;
 public class ErrorMessage {
 
     public static final String NOT_FOUND_MEMBER = "사용자를 찾을 수 없습니다.";
+    public static final String NOT_FOUND_HIKING = "등산 정보를 찾을 수 없습니다.";
 }
