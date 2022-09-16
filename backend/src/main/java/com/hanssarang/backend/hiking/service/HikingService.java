d3078b88228850d71fb508228bbb3673bfc891dc
jeonyuneo <piq2255@gmail.com>
2022-09-16 15:55:27 +0900

    fix: HikingService - 등산목록 조회 반환 타입 수정

--- backend/src/main/java/com/hanssarang/backend/hiking/service/HikingService.java
+++ backend/src/main/java/com/hanssarang/backend/hiking/service/HikingService.java
@@ -4,10 +4,12 @@ import com.hanssarang.backend.hiking.controller.dto.HikingListResponse;
 import com.hanssarang.backend.hiking.controller.dto.HikingResponse;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class HikingService {
 
-    public HikingListResponse getHikings(int memberId) {
+    public List<HikingListResponse> getHikings(int memberId) {
         return null;
     }
 
