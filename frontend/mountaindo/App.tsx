1da5a25b3447e27b8dd1e3e48a29ee5ec7c6060e
Shin Young Jei <zerojei0423@naver.com>
2022-09-16 14:49:37 +0900

    refactor: 불필요한 reducer 제거

--- frontend/mountaindo/App.tsx
+++ frontend/mountaindo/App.tsx
@@ -2,11 +2,14 @@ import * as React from 'react';
 import {Provider} from 'react-redux';
 import store from './src/store';
 import AppInner from './AppInner';
+import {NavigationContainer} from '@react-navigation/native';
 
 function App() {
   return (
     <Provider store={store}>
-      <AppInner />
+      <NavigationContainer>
+        <AppInner />
+      </NavigationContainer>
     </Provider>
   );
 }
