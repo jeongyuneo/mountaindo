e56ea0a6be06a37809241a377482ecf416b24ec6
jeonyuneo <piq2255@gmail.com>
2022-09-16 18:09:24 +0900

    feat: ErrorMessage - 등산 정보 저장 실패 메시지 추가

--- backend/src/main/java/com/hanssarang/backend/common/domain/ErrorMessage.java
+++ backend/src/main/java/com/hanssarang/backend/common/domain/ErrorMessage.java
@@ -4,4 +4,5 @@ public class ErrorMessage {
 
     public static final String NOT_FOUND_MEMBER = "사용자를 찾을 수 없습니다.";
     public static final String NOT_FOUND_HIKING = "등산 정보를 찾을 수 없습니다.";
+    public static final String FAIL_TO_CREATE_HIKING = "등산 정보 저장을 실패했습니다.";
 }
