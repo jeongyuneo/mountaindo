45cac721f7e9a10c173f3f251a2b568b61d2535e
신영제 <zerojei0423@naver.com>
2022-09-16 10:57:37 +0900

    refactor: MainScreenProps 타입이름 변경

--- frontend/mountaindo/src/pages/Main.tsx
+++ frontend/mountaindo/src/pages/Main.tsx
@@ -2,8 +2,8 @@ import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import React from 'react';
 import {Text, View} from 'react-native';
 import {LoggedInParamList} from '../../AppInner';
-type SignInScreenProps = NativeStackScreenProps<LoggedInParamList, 'Main'>;
-function Main({navigation}: SignInScreenProps) {
+type MainInScreenProps = NativeStackScreenProps<LoggedInParamList, 'Main'>;
+function Main({navigation}: MainInScreenProps) {
   return (
     <View>
       <Text onPress={() => navigation.navigate('MyPage')}>Test</Text>
