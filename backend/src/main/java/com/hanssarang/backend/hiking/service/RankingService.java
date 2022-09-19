a30bc8de130223e7add1b78b3c0ae2fe2d734f6c
어정윤 <piq2255@naver.com>
2022-09-19 20:38:27 +0900

    feat: RankingService 추가

---
+++ backend/src/main/java/com/hanssarang/backend/hiking/service/RankingService.java
@@ -0,0 +1,12 @@
+package com.hanssarang.backend.hiking.service;
+
+import com.hanssarang.backend.hiking.controller.dto.RankingResponse;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RankingService {
+
+    public RankingResponse getRankings(int memberId) {
+        return null;
+    }
+}
