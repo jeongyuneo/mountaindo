71f5de8e1e15c874f377732211651eb283d5722a
rlamsp234 <rlamsp234@naver.com>
2022-09-19 12:14:13 +0900

    fix: 가입 환영 페이지 수정

--- frontend/mountaindo/src/pages/Welcome.tsx
+++ frontend/mountaindo/src/pages/Welcome.tsx
@@ -2,6 +2,8 @@ import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import React from 'react';
 import {Pressable, StyleSheet, Text, View} from 'react-native';
 import {RootStackParamList} from '../../AppInner';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {faArrowRight} from '@fortawesome/free-solid-svg-icons';
 
 type WelcomeScreenProps = NativeStackScreenProps<RootStackParamList, 'Welcome'>;
 
@@ -14,10 +16,15 @@ function Welcome({navigation}: WelcomeScreenProps) {
           간단한 설문 조사를 통해 등산 코스를 추천해드려요!
         </Text>
         <Text style={styles.title}>설문조사 하러가기</Text>
+
         <Pressable
           style={styles.arrowButton}
-          onPress={() => navigation.navigate('Survey1')}>
-          <Text>go</Text>
+          onPress={() => navigation.navigate('Survey')}>
+          <FontAwesomeIcon
+            icon={faArrowRight}
+            size={30}
+            style={styles.arrowIcon}
+          />
         </Pressable>
       </View>
     </View>
@@ -41,7 +48,15 @@ const styles = StyleSheet.create({
     fontSize: 15,
   },
   arrowButton: {
-    alignItems: 'center',
+    padding: 20,
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    borderRadius: 50,
+    marginTop: 80,
+    backgroundColor: 'black',
+  },
+  arrowIcon: {
+    color: 'white',
   },
 });
 
