b9dba96ce46d9d25958076ed922819983215f722
어정윤 <piq2255@naver.com>
2022-09-19 21:46:13 +0900

    feat: RankingService - 랭킹 내 사용자 검색 추가

--- backend/src/main/java/com/hanssarang/backend/hiking/service/RankingService.java
+++ backend/src/main/java/com/hanssarang/backend/hiking/service/RankingService.java
@@ -1,12 +1,17 @@
 package com.hanssarang.backend.hiking.service;
 
 import com.hanssarang.backend.hiking.controller.dto.RankingResponse;
+import com.hanssarang.backend.hiking.controller.dto.RankingSearchResponse;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RankingService {
 
     public RankingResponse getRankings(int memberId) {
+        return null;
+    }
+
+    public RankingSearchResponse searchRanking(String nickname) {
         return null;
     }
 }
