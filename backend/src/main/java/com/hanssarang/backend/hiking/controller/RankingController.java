ad6bac73adec610f9bb7da24c0f81c49d66d151a
어정윤 <piq2255@naver.com>
2022-09-19 21:46:02 +0900

    feat: RankingController - 랭킹 내 사용자 검색 추가

--- backend/src/main/java/com/hanssarang/backend/hiking/controller/RankingController.java
+++ backend/src/main/java/com/hanssarang/backend/hiking/controller/RankingController.java
@@ -1,13 +1,11 @@
 package com.hanssarang.backend.hiking.controller;
 
 import com.hanssarang.backend.hiking.controller.dto.RankingResponse;
+import com.hanssarang.backend.hiking.controller.dto.RankingSearchResponse;
 import com.hanssarang.backend.hiking.service.RankingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -19,5 +17,10 @@ public class RankingController {
     @GetMapping
     public ResponseEntity<RankingResponse> getRankings(@RequestHeader("Authorization") String token) {
         return ResponseEntity.ok(rankingService.getRankings(1));
+    }
+
+    @GetMapping("/1")
+    public ResponseEntity<RankingSearchResponse> searchRanking(@RequestHeader("Authorization") String token, @RequestParam String nickname) {
+        return ResponseEntity.ok(rankingService.searchRanking(nickname));
     }
 }
