c5402c380fa719b1bd3ad03e4481605eda9b2e81
DESKTOP-KVCQHCD\SSAFY <pbsu1213@hanmail.net>
2022-09-19 17:24:30 +0900

    feat: MemberService - 일반 회원 가입 추가

---
+++ backend/src/main/java/com/hanssarang/backend/member/service/MemberService.java
@@ -0,0 +1,15 @@
+package com.hanssarang.backend.member.service;
+
+import com.hanssarang.backend.member.controller.dto.MemberEmailResponse;
+import com.hanssarang.backend.member.controller.dto.MemberSignUpRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberService {
+    public MemberEmailResponse checkEmail(String memberEmail) {
+        return null;
+    }
+    public void signUpNormal(MemberSignUpRequest memberSignUpRequest) {
+
+    }
+}
