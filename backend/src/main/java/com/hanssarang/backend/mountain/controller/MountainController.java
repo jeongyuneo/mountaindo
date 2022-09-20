39b32b0b326075b0e52bda569bcac26269d7e025
jeonyuneo <piq2255@gmail.com>
2022-09-20 16:07:02 +0900

    refactor: MountainController 어노테이션 선언 순서 수정

--- backend/src/main/java/com/hanssarang/backend/mountain/controller/MountainController.java
+++ backend/src/main/java/com/hanssarang/backend/mountain/controller/MountainController.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
 @RequiredArgsConstructor
+@RestController
 @RequestMapping("/api/v1/mountains")
 public class MountainController {
 
