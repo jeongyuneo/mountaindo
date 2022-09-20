f2e987be46646be05f0ce1cbef282d13878d0d9f
DESKTOP-KVCQHCD\SSAFY <pbsu1213@hanmail.net>
2022-09-20 10:01:25 +0900

    feat: ErrorMessage 추가

---
+++ backend/src/main/java/com/hanssarang/backend/common/domain/ErrorMessage.java
@@ -0,0 +1,9 @@
+package com.hanssarang.backend.common.domain;
+
+public class ErrorMessage {
+
+    public static final String FAIL_TO_CHECK_EMAIL = "중복된 이메일이 있습니다.";
+    public static final String FAIL_TO_SIGNUP = "가입에 실패했습니다.";
+    public static final String FAIL_TO_SURVEY = "사전 설문조사에 실패했습니다.";
+
+}
