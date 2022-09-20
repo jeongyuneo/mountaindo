ace5148cb52a40c33b4f9b02d974529c06bc40c6
jeonyuneo <piq2255@gmail.com>
2022-09-20 16:06:09 +0900

    refactor: 불필요 클래스 삭제

--- backend/src/main/java/com/hanssarang/backend/mountain/domain/MountainRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hanssarang.backend.mountain.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MountainRepository extends JpaRepository<Mountain, Long> {
-
-}
