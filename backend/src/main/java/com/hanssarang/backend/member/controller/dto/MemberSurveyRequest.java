f22df2704e73b4c486327263551e364872bab596
DESKTOP-KVCQHCD\SSAFY <pbsu1213@hanmail.net>
2022-09-20 09:15:10 +0900

    feat: MemberSurveyRequest 추가

---
+++ backend/src/main/java/com/hanssarang/backend/member/controller/dto/MemberSurveyRequest.java
@@ -0,0 +1,17 @@
+package com.hanssarang.backend.member.controller.dto;
+
+import lombok.*;
+
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MemberSurveyRequest {
+
+    private String myLevel;
+    private String visitedMountain;
+    private String mountainLocation;
+    private String mountainStyle;
+    private String climbingTime;
+    private String mountainType;
+}
