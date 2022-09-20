ad6610d1775fda8acad4b66633008a5e007e7457
DESKTOP-KVCQHCD\SSAFY <pbsu1213@hanmail.net>
2022-09-20 09:14:23 +0900

    feat: MemberService - 사전 설문조사 추가

--- backend/src/main/java/com/hanssarang/backend/member/service/MemberService.java
+++ backend/src/main/java/com/hanssarang/backend/member/service/MemberService.java
@@ -2,6 +2,7 @@ package com.hanssarang.backend.member.service;
 
 import com.hanssarang.backend.member.controller.dto.MemberEmailResponse;
 import com.hanssarang.backend.member.controller.dto.MemberSignUpRequest;
+import com.hanssarang.backend.member.controller.dto.MemberSurveyRequest;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,7 +10,12 @@ public class MemberService {
     public MemberEmailResponse checkEmail(String memberEmail) {
         return null;
     }
+
     public void signUpNormal(MemberSignUpRequest memberSignUpRequest) {
+
+    }
+
+    public void preSurvey(MemberSurveyRequest memberSurveyRequest) {
 
     }
 }
