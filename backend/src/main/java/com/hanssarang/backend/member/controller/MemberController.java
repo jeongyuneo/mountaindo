872339d86aad36ea5ad092783757689352e0b68d
DESKTOP-KVCQHCD\SSAFY <pbsu1213@hanmail.net>
2022-09-21 11:20:47 +0900

    feat: MemberController - 닉네임 중복체크 및 회원정보 조회, 수정, 탈퇴 비밀번호 재설정 추가

--- backend/src/main/java/com/hanssarang/backend/member/controller/MemberController.java
+++ backend/src/main/java/com/hanssarang/backend/member/controller/MemberController.java
@@ -1,7 +1,6 @@
 package com.hanssarang.backend.member.controller;
 
-import com.hanssarang.backend.member.controller.dto.MemberEmailResponse;
-import com.hanssarang.backend.member.controller.dto.MemberSignUpRequest;
+import com.hanssarang.backend.member.controller.dto.*;
 import com.hanssarang.backend.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -14,9 +13,14 @@ public class MemberController {
 
     private final MemberService memberService;
 
-    @GetMapping()
-    public ResponseEntity<MemberEmailResponse> checkEmail(@RequestHeader("Authorization") String token) {
-        return ResponseEntity.ok().body(memberService.checkEmail("ssafy"));
+    @GetMapping("/email")
+    public ResponseEntity<MemberEmailResponse> checkEmail(@RequestParam String memberEmail) {
+        return ResponseEntity.ok().body(memberService.checkEmail(memberEmail));
+    }
+
+    @GetMapping("/nickname")
+    public ResponseEntity<MemberNicknameResponse> checkNickname(@RequestParam String nickname) {
+        return ResponseEntity.ok().body(memberService.checkNickname(nickname));
     }
 
     @PostMapping("/signup")
@@ -24,8 +28,44 @@ public class MemberController {
         memberService.signUpNormal(memberSignUpRequest);
         return ResponseEntity.ok().build();
     }
-//    @PostMapping("/signup/normal")
-//    public ResponseEntity<?> signUpNormal(@Valid @RequestBody Member member) throws Exception {
-//        return null;
-//    }
+
+    @PostMapping("/survey")
+    public ResponseEntity<Void> preSurvey(@RequestHeader("Authorization") String token, @RequestBody MemberSurveyRequest memberSurveyRequest) {
+        memberService.createPreSurvey(memberSurveyRequest);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/getid")
+    public ResponseEntity<MemberEmailResponse> getMemberEmail(@RequestBody MemberGetIdRequest memberGetIdRequest) {
+        return ResponseEntity.ok().body(memberService.getMemberEmail(memberGetIdRequest));
+    }
+
+    @GetMapping("/info/{memberId}")
+    public ResponseEntity<MemberResponse> getMember(@RequestHeader("Authorization") String token, @PathVariable int memberId) {
+        return ResponseEntity.ok(memberService.getMemeber(memberId));
+    }
+
+    @PatchMapping("/update/{memberId}")
+    public ResponseEntity<Void> updateMember(@RequestHeader("Authorization") String token,@PathVariable int memberId, @RequestBody MemberRequest memberRequest) {
+        memberService.updateMember(memberId, memberRequest);
+        return ResponseEntity.ok().build();
+    }
+
+    @PatchMapping("/update/password/{memberId}")
+    public ResponseEntity<Void> updatePassword(@PathVariable int memberId, @RequestBody MemberUpdatePasswordRequest memberUpdatePasswordRequest) {
+        memberService.updatePassword(memberId, memberUpdatePasswordRequest);
+        return ResponseEntity.ok().build();
+    }
+
+    @PatchMapping("/update/mypage/password/{memberId}")
+    public ResponseEntity<Void> updateMyPagePassword(@PathVariable int memberId, @RequestBody MemberPasswordRequest memberPasswordRequest) {
+        memberService.updateMyPagePassword(memberId, memberPasswordRequest);
+        return ResponseEntity.ok().build();
+    }
+
+    @PatchMapping("/delete/{memberId}")
+    public ResponseEntity<Void> deleteMember(@PathVariable int memberId, @RequestParam String deleteMessage) {
+        memberService.deleteMember(memberId, deleteMessage);
+        return ResponseEntity.ok().build();
+    }
 }
