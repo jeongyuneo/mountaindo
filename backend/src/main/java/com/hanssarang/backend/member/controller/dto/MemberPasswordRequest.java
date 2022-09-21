e5ed063f62183eaa6ba64f1681bb55b0f7e07767
DESKTOP-KVCQHCD\SSAFY <pbsu1213@hanmail.net>
2022-09-21 11:16:06 +0900

    feat: MemberPasswordRequest 추가

---
+++ backend/src/main/java/com/hanssarang/backend/member/controller/dto/MemberPasswordRequest.java
@@ -0,0 +1,15 @@
+package com.hanssarang.backend.member.controller.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class MemberPasswordRequest {
+
+    private String password;
+}
