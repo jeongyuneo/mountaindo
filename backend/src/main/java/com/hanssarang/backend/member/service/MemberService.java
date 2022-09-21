ca934a0aa89f74c6d0a5368bc134d5ddc2d58786
DESKTOP-KVCQHCD\SSAFY <pbsu1213@hanmail.net>
2022-09-21 11:19:28 +0900

    feat: MemberService - 닉네임 중복체크 및 회원정보 조회, 수정, 비밀번호 재설정 추가

--- backend/src/main/java/com/hanssarang/backend/member/service/MemberService.java
+++ backend/src/main/java/com/hanssarang/backend/member/service/MemberService.java
@@ -1,8 +1,6 @@
 package com.hanssarang.backend.member.service;
 
-import com.hanssarang.backend.member.controller.dto.MemberEmailResponse;
-import com.hanssarang.backend.member.controller.dto.MemberSignUpRequest;
-import com.hanssarang.backend.member.controller.dto.MemberSurveyRequest;
+import com.hanssarang.backend.member.controller.dto.*;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,11 +9,39 @@ public class MemberService {
         return null;
     }
 
+    public MemberNicknameResponse checkNickname(String nickname) {
+        return null;
+    }
+
     public void signUpNormal(MemberSignUpRequest memberSignUpRequest) {
 
     }
 
-    public void preSurvey(MemberSurveyRequest memberSurveyRequest) {
+    public void createPreSurvey(MemberSurveyRequest memberSurveyRequest) {
 
+    }
+
+    public MemberEmailResponse getMemberEmail(MemberGetIdRequest memberGetIdRequest) {
+        return null;
+    }
+
+    public MemberResponse getMemeber(int memberId) {
+        return null;
+    }
+
+    public void updateMember(int memberId, MemberRequest memberRequest) {
+    }
+
+    public void updatePassword(int memberId, MemberUpdatePasswordRequest memberUpdatePasswordRequest) {
+    }
+
+    public void updateMyPagePassword(int memberId, MemberPasswordRequest memberPasswordRequest) {
+    }
+
+    public void deleteMember(int memberId, String deleteMessage) {
+    }
+
+    public MemberLoginResponse getMemberLogin(MemberLoginRequest memberLoginRequest) {
+        return null;
     }
 }
