eaf4193ef525a3b542faf6094df404177d9bff47
DESKTOP-KVCQHCD\SSAFY <pbsu1213@hanmail.net>
2022-09-21 13:24:20 +0900

    refactor: MemberResponse - AccessLevel 추가

--- backend/src/main/java/com/hanssarang/backend/member/controller/dto/MemberResponse.java
+++ backend/src/main/java/com/hanssarang/backend/member/controller/dto/MemberResponse.java
@@ -1,14 +1,11 @@
 package com.hanssarang.backend.member.controller.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Getter
 @Builder
-@AllArgsConstructor
-@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MemberResponse {
 
     private String email;
@@ -18,5 +15,4 @@ public class MemberResponse {
     private String address;
     private String nickName;
     private String profilePicture;
-
 }
