dcdc48e419205343aeffe4dc1d7e3968322fd949
DESKTOP-KVCQHCD\SSAFY <pbsu1213@hanmail.net>
2022-09-22 13:44:18 +0900

    refactor: NicknameResponse - 코드 리뷰 적용

--- backend/src/main/java/com/hanssarang/backend/member/controller/dto/NicknameResponse.java
+++ backend/src/main/java/com/hanssarang/backend/member/controller/dto/NicknameResponse.java
@@ -6,7 +6,7 @@ import lombok.*;
 @Builder
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class MemberNicknameResponse {
+public class NicknameResponse {
 
     private String nickname;
 }
