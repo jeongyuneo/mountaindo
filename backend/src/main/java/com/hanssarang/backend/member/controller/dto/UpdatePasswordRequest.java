3113576bdd77143407aa226c11e2723df1a593a4
DESKTOP-KVCQHCD\SSAFY <pbsu1213@hanmail.net>
2022-09-22 13:45:48 +0900

    refactor: UpdatePasswordRequest - 코드 리뷰 적용

--- backend/src/main/java/com/hanssarang/backend/member/controller/dto/UpdatePasswordRequest.java
+++ backend/src/main/java/com/hanssarang/backend/member/controller/dto/UpdatePasswordRequest.java
@@ -6,7 +6,7 @@ import lombok.*;
 @Builder
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class MemberUpdatePasswordRequest {
+public class UpdatePasswordRequest {
 
     private String email;
     private String name;
