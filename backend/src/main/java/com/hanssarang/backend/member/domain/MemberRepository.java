336fdc2dd2a9d7b5553e10461bc595d5524ba7f2
giyoon27 <giyoon27@naver.com>
2022-09-24 23:37:47 +0900

    feat: MemberRepository 추가

--- backend/src/main/java/com/hanssarang/backend/member/domain/MemberRepository.java
+++ backend/src/main/java/com/hanssarang/backend/member/domain/MemberRepository.java
@@ -2,5 +2,20 @@ package com.hanssarang.backend.member.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
+import java.time.LocalDate;
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Integer> {
+
+    Optional<Member> findById(int memberId);
+
+    Optional<Member> findByNameAndBirthAndPhoneAndIsActiveTrue(String name, LocalDate birth, String phone);
+
+    Optional<Member> findByEmailAndName(String email, String name);
+
+    Optional<Member> findByEmailAndIsActiveTrue(String email);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByNickname(String nickname);
 }
