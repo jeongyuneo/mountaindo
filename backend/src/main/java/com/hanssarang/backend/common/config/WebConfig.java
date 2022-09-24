a8567921f7fa8e593315a885fad20f1ad9b192bb
giyoon27 <giyoon27@naver.com>
2022-09-25 08:51:50 +0900

    feat: WebConfig 추가

---
+++ backend/src/main/java/com/hanssarang/backend/common/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.hanssarang.backend.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private static final String[] EXCLUDED_PATHS = new String[]{
+            "/api/v1/members/**",
+    };
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .exposedHeaders("Authorization")
+                .maxAge(6000);
+    }
+}
