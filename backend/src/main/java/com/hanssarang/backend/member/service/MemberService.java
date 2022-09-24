d9f10e9f81fce72dd83059bda31621a98d42e09d
giyoon27 <giyoon27@naver.com>
2022-09-25 08:33:19 +0900

    refactor: 파라미터 개행 추가

--- backend/src/main/java/com/hanssarang/backend/member/service/MemberService.java
+++ backend/src/main/java/com/hanssarang/backend/member/service/MemberService.java
@@ -66,7 +66,8 @@ public class MemberService {
     }
 
     public EmailResponse getMemberEmail(FindingEmailRequest findingEmailRequest) {
-        Member member = memberRepository.findByNameAndBirthAndPhoneAndIsActiveTrue(findingEmailRequest.getName(),
+        Member member = memberRepository.findByNameAndBirthAndPhoneAndIsActiveTrue(
+                        findingEmailRequest.getName(),
                         findingEmailRequest.getBirth(),
                         findingEmailRequest.getPhone())
                 .orElseThrow(() -> new NotFoundException(FAIL_TO_FIND_EMAIL));
