b9ad8f329ee2088447f628095136a5c05b7f7cea
giyoon27 <giyoon27@naver.com>
2022-09-25 08:52:21 +0900

    feat: WebSecurityConfig 추가

---
+++ backend/src/main/java/com/hanssarang/backend/common/config/WebSecurityConfig.java
@@ -0,0 +1,30 @@
+package com.hanssarang.backend.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.antMatcher("/**")
+                .authorizeRequests()
+                .antMatchers("/**").permitAll()
+                .and()
+                .csrf().disable()
+                .formLogin().disable()
+                .headers().frameOptions().sameOrigin();
+    }
+}
