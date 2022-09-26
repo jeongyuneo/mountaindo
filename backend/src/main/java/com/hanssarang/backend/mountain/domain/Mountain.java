c0ce43adf6c8ddaf7519f79c4297c46136cdaed9
jeonyuneo <piq2255@gmail.com>
2022-09-26 11:19:09 +0900

    style: 전체 엔터티 SuperBuilder 어노테이션 적용

--- backend/src/main/java/com/hanssarang/backend/mountain/domain/Mountain.java
+++ backend/src/main/java/com/hanssarang/backend/mountain/domain/Mountain.java
@@ -3,15 +3,17 @@ package com.hanssarang.backend.mountain.domain;
 import com.hanssarang.backend.common.domain.Address;
 import com.hanssarang.backend.common.domain.BaseEntity;
 import lombok.AccessLevel;
-import lombok.Builder;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 import javax.persistence.*;
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
+@SuperBuilder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AttributeOverride(name = "id", column = @Column(name = "mountain_id"))
 @Entity
@@ -27,16 +29,4 @@ public class Mountain extends BaseEntity {
 
     @OneToMany(mappedBy = "mountain", cascade = {CascadeType.PERSIST, CascadeType.MERGE})
     private List<Trail> trails;
-
-    @Builder
-    public Mountain(Long id, LocalDateTime createdDate, LocalDateTime lastModifiedDate, LocalDateTime deletedDate, boolean isActive,
-                    String name, String code, double height, String imageUrl, Address address, List<Trail> trails) {
-        super(id, createdDate, lastModifiedDate, deletedDate, isActive);
-        this.name = name;
-        this.code = code;
-        this.height = height;
-        this.imageUrl = imageUrl;
-        this.address = address;
-        this.trails = trails;
-    }
 }
--- backend/src/main/java/com/hanssarang/backend/mountain/domain/Trail.java
+++ backend/src/main/java/com/hanssarang/backend/mountain/domain/Trail.java
@@ -2,16 +2,18 @@ package com.hanssarang.backend.mountain.domain;
 
 import com.hanssarang.backend.common.domain.BaseEntity;
 import lombok.AccessLevel;
-import lombok.Builder;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 import org.geolatte.geom.LineString;
 
 import javax.persistence.*;
-import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 @Getter
+@SuperBuilder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AttributeOverride(name = "id", column = @Column(name = "trail_id"))
 @Entity
@@ -32,18 +34,4 @@ public class Trail extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "mountain_id")
     private Mountain mountain;
-
-    @Builder
-    public Trail(Long id, LocalDateTime createdDate, LocalDateTime lastModifiedDate, LocalDateTime deletedDate, boolean isActive,
-                 String name, int length, LocalTime goingUpTime, LocalTime goingDownTime, String risk, Level level, LineString path, Mountain mountain) {
-        super(id, createdDate, lastModifiedDate, deletedDate, isActive);
-        this.name = name;
-        this.length = length;
-        this.goingUpTime = goingUpTime;
-        this.goingDownTime = goingDownTime;
-        this.risk = risk;
-        this.level = level;
-        this.path = path;
-        this.mountain = mountain;
-    }
 }
