ce2075cfe374404bbf1cddfae2028e46d8e864a6
jeonyuneo <piq2255@gmail.com>
2022-09-26 13:55:04 +0900

    feat: UpdateResponse 추가

---
+++ backend/src/main/java/com/hanssarang/backend/member/controller/dto/UpdateResponse.java
@@ -0,0 +1,14 @@
+package com.hanssarang.backend.member.controller.dto;
+
+import lombok.*;
+
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UpdateResponse {
+
+    private String nickname;
+    private String imageUrl;
+    private String token;
+}
