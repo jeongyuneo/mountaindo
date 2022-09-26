11567bd3e770d5a8154f14d7bfa4e6f7f7fbdfd3
jeonyuneo <piq2255@gmail.com>
2022-09-26 14:03:04 +0900

    feat: WrongAccessException 추가

---
+++ backend/src/main/java/com/hanssarang/backend/common/exception/WrongAccessException.java
@@ -0,0 +1,8 @@
+package com.hanssarang.backend.common.exception;
+
+public class WrongAccessException extends CommonException {
+
+    public WrongAccessException(String message) {
+        super(message);
+    }
+}
