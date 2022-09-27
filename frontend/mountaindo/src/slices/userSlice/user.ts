bf4c7f19fcd19d4158d039a8f58eb791b21bcef6
rlamsp234 <rlamsp234@naver.com>
2022-09-27 22:57:17 +0900

    feat: 회원가입 API 연동

--- frontend/mountaindo/src/slices/userSlice/user.ts
+++ frontend/mountaindo/src/slices/userSlice/user.ts
@@ -25,6 +25,51 @@ export const login = createAsyncThunk(
   },
 );
 
+export const signUp = createAsyncThunk(
+  'userSlice/signUp',
+  async (args: any, {rejectWithValue}) => {
+    try {
+      console.log('SIGNUP_TRY ==>', args);
+
+      const response = await axiosService.post('api/v1/members', {
+        email: args.email,
+        password: args.password,
+        name: args.name,
+        birth: args.check,
+        phone: args.phoneNumber,
+        address: {
+          si: args.selectedCity,
+          gu: args.selectedCity2,
+          dong: args.selectedCity2,
+          fullAddress: '경기도',
+        },
+        nickname: args.nickName,
+      });
+      return response.data;
+    } catch (err: any) {
+      return rejectWithValue(err.response);
+    }
+  },
+);
+
+export const checkCertification = createAsyncThunk(
+  'userSlice/checkCertification',
+  async (args: any, {rejectWithValue}) => {
+    try {
+      console.log('CHECKCERTIFICATION_TRY ==>', args);
+
+      const response = await axiosService.get('api/v1/members/email?', {
+        params: {
+          email: args.email,
+        },
+      });
+      return response.data;
+    } catch (err: any) {
+      return rejectWithValue(err.response);
+    }
+  },
+);
+
 const userSlice = createSlice({
   name: 'user',
   initialState,
@@ -45,6 +90,24 @@ const userSlice = createSlice({
       })
       .addCase(login.rejected, (state, {payload}) => {
         console.log('LoginRejected ==>', payload);
+      })
+      .addCase(signUp.pending, (state, {payload}) => {
+        console.log('SignUp Pending ==>', payload);
+      })
+      .addCase(signUp.fulfilled, (state, {payload}) => {
+        console.log('SignUp Fulfilled ==>', payload);
+      })
+      .addCase(signUp.rejected, (state, {payload}) => {
+        console.log('SignUp Rejected ==>', payload);
+      })
+      .addCase(checkCertification.pending, (state, {payload}) => {
+        console.log('CheckCertification Pending ==>', payload);
+      })
+      .addCase(checkCertification.fulfilled, (state, {payload}) => {
+        console.log('CheckCertification Fullfilled ==>', payload);
+      })
+      .addCase(checkCertification.rejected, (state, {payload}) => {
+        console.log('CheckCertification Rejected ==>', payload);
       });
   },
 });
