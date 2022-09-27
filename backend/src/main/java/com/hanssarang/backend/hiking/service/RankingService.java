5c5011e3cf728aa919a0d5b0aaaba0e310461248
jeonyuneo <piq2255@gmail.com>
2022-09-27 23:50:15 +0900

    feat: RankingService - 전체 등산 랭킹 검색 기능 구현

--- backend/src/main/java/com/hanssarang/backend/hiking/service/RankingService.java
+++ backend/src/main/java/com/hanssarang/backend/hiking/service/RankingService.java
@@ -2,17 +2,38 @@ package com.hanssarang.backend.hiking.service;
 
 import com.hanssarang.backend.hiking.controller.dto.RankingListResponse;
 import com.hanssarang.backend.hiking.controller.dto.RankingResponse;
+import com.hanssarang.backend.member.domain.Member;
+import com.hanssarang.backend.member.domain.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@RequiredArgsConstructor
 @Service
 public class RankingService {
+
+    private final MemberRepository memberRepository;
 
     public RankingListResponse getRankings(int memberId) {
         return null;
     }
 
-    public RankingResponse searchRanking(String keyword) {
-        return null;
+    public List<RankingResponse> searchRanking(String keyword) {
+        List<Member> members = memberRepository.findAll();
+        members.sort(Comparator.comparing(Member::getAccumulatedHeight).reversed());
+        return IntStream.rangeClosed(1, members.size())
+                .filter(ranking -> members.get(ranking - 1).getNickname().contains(keyword))
+                .mapToObj(ranking -> RankingResponse.builder()
+                        .ranking(ranking)
+                        .nickname(members.get(ranking - 1).getNickname())
+                        .imageUrl(members.get(ranking - 1).getImageUrl())
+                        .accumulatedHeight(members.get(ranking - 1).getAccumulatedHeight())
+                        .build())
+                .collect(Collectors.toList());
     }
 
     public RankingListResponse getRankingsOfMountain(int memberId, int mountainId) {
