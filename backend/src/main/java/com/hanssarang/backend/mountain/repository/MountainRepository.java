36055c67cac367aaae4bcbfe0606a6b67a523917
DESKTOP-KVCQHCD\SSAFY <pbsu1213@hanmail.net>
2022-09-28 21:24:14 +0900

    feat: MountainRepository - 인기 top10 산 기능 추가

--- backend/src/main/java/com/hanssarang/backend/mountain/repository/MountainRepository.java
+++ backend/src/main/java/com/hanssarang/backend/mountain/repository/MountainRepository.java
@@ -3,7 +3,6 @@ package com.hanssarang.backend.mountain.repository;
 import com.hanssarang.backend.mountain.domain.Mountain;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -21,5 +20,15 @@ public interface MountainRepository extends JpaRepository<Mountain, Integer> {
             "order by count(m.mountain_id) desc ", nativeQuery = true)
     List<Mountain> findAllPopularity(String sort);
 
-public interface MountainRepository extends JpaRepository<Mountain, Long> {
+    @Query(value = "select m.* " +
+            "from mountain m " +
+            "inner join (select h.trail_id, count(h.trail_id) count, t.mountain_id " +
+            "            from hiking h " +
+            "            inner join trail t " +
+            "            on t.trail_id = h.trail_id " +
+            "            group by h.trail_id) v " +
+            "on m.mountain_id = v.mountain_id " +
+            "group by m.mountain_id " +
+            "LIMIT 10", nativeQuery = true)
+    List<Mountain> findIsHot();
 }
