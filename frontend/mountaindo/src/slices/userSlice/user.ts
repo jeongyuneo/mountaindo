6832cf7f519bac4bdd4ba9182ad3158f4eeb7199
Shin Young Jei <zerojei0423@naver.com>
2022-09-28 11:15:08 +0900

    feat: 로그인 및 토큰 API 연동

--- frontend/mountaindo/src/slices/userSlice/user.ts
+++ frontend/mountaindo/src/slices/userSlice/user.ts
@@ -1,19 +1,28 @@
 import {createAsyncThunk, createSlice} from '@reduxjs/toolkit';
-import Config from 'react-native-config';
 import axiosService from '../../store/axiosService';
 const initialState = {
-  name: '',
-  email: '',
-  accessToken: '',
+  isLoggedIn: false,
 };
 
+// 회원정보 API
+export const userInfo = createAsyncThunk(
+  'userSlice/userInfo',
+  async (args: any, {rejectWithValue}) => {
+    try {
+      const response = await axiosService.get('/api/v1/members');
+      return response.data;
+    } catch (err: any) {
+      return rejectWithValue(err.response);
+    }
+  },
+);
+
+// 로그인 API
 export const login = createAsyncThunk(
   'userSlice/login',
   async (args: any, {rejectWithValue}) => {
     try {
       console.log('LOGIN_TRY ==>', args);
-      console.log('CONFIG ===> ', Config.REACT_APP_BE_HOST);
-
       const response = await axiosService.post('/api/v1/members/login', {
         email: args.email,
         password: args.password,
@@ -29,10 +38,8 @@ const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {
-    setUser(state, action) {
-      state.email = action.payload.email;
-      state.name = action.payload.name;
-      state.accessToken = action.payload.accessToken;
+    setLogout(state, action) {
+      state.isLoggedIn = false;
     },
   },
   extraReducers: builder => {
@@ -42,9 +49,19 @@ const userSlice = createSlice({
       })
       .addCase(login.fulfilled, (state, {payload}) => {
         console.log('LoginFulfilled ==> ', payload);
+        if (payload.token) {
+          state.isLoggedIn = true;
+        }
       })
       .addCase(login.rejected, (state, {payload}) => {
         console.log('LoginRejected ==>', payload);
+        state.isLoggedIn = false;
+      })
+      .addCase(userInfo.fulfilled, (state, {payload}) => {
+        console.log('userInfoFulfilled ==> ', payload);
+      })
+      .addCase(userInfo.rejected, (state, {payload}) => {
+        console.log('userInfoRejected ==>', payload);
       });
   },
 });
