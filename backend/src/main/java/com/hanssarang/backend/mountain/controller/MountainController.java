18d8ea05397e65b3e5e6a2fcf564343747092501
DESKTOP-KVCQHCD\SSAFY <pbsu1213@hanmail.net>
2022-09-28 21:28:36 +0900

    refactor: MountainController - ResponseEntity.ok.body() 형식으로 수정

--- backend/src/main/java/com/hanssarang/backend/mountain/controller/MountainController.java
+++ backend/src/main/java/com/hanssarang/backend/mountain/controller/MountainController.java
@@ -5,11 +5,13 @@ import com.hanssarang.backend.mountain.controller.dto.MountainResponse;
 import com.hanssarang.backend.mountain.controller.dto.TrailResponse;
 import com.hanssarang.backend.mountain.service.MountainService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/mountains")
@@ -18,32 +20,32 @@ public class MountainController {
     private final MountainService mountainService;
 
     @GetMapping
-    public ResponseEntity<List<MountainListResponse>> getMountains(@RequestHeader("Authorization") String token, @RequestParam(required = false) String sort) {
-        return ResponseEntity.ok(mountainService.getMountains(sort));
+    public ResponseEntity<List<MountainListResponse>> getMountains(@RequestHeader("Authorization") String token, @RequestParam String sort) {
+        return ResponseEntity.ok().body(mountainService.getMountains(sort));
     }
 
     @GetMapping("/{mountainId}")
     public ResponseEntity<MountainResponse> getMountain(@RequestHeader("Authorization") String token, @PathVariable int mountainId) {
-        return ResponseEntity.ok(mountainService.getMountain(mountainId));
+        return ResponseEntity.ok().body(mountainService.getMountain(mountainId));
     }
 
     @GetMapping("/trail/{trailId}")
     public ResponseEntity<TrailResponse> getTrail(@RequestHeader("Authorization") String token, @PathVariable int trailId) {
-        return ResponseEntity.ok(mountainService.getTrail(trailId));
+        return ResponseEntity.ok().body(mountainService.getTrail(trailId));
     }
 
     @GetMapping("/search/1")
     public ResponseEntity<List<MountainListResponse>> searchMountainOrTrail(@RequestHeader("Authorization") String token, @RequestParam String keyword) {
-        return ResponseEntity.ok(mountainService.searchMountainOrTrail(keyword));
+        return ResponseEntity.ok().body(mountainService.searchMountainOrTrail(keyword));
     }
 
     @GetMapping("/search/2")
     public ResponseEntity<List<MountainListResponse>> searchMountain(@RequestHeader("Authorization") String token, @RequestParam String keyword) {
-        return ResponseEntity.ok(mountainService.searchMountain(keyword));
+        return ResponseEntity.ok().body(mountainService.searchMountain(keyword));
     }
 
     @GetMapping("/search/3")
     public ResponseEntity<List<MountainListResponse>> searchTrail(@RequestHeader("Authorization") String token, @RequestParam String keyword) {
-        return ResponseEntity.ok(mountainService.searchTrail(keyword));
+        return ResponseEntity.ok().body(mountainService.searchTrail(keyword));
     }
 }
