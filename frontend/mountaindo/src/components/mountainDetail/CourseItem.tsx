b6290523b959e25e02b082a5df770428277de6ff
ska05142 <ska05142@naver.com>
2022-09-29 15:34:33 +0900

    design: 산 상세 페이지 폰트 추가

--- frontend/mountaindo/src/components/mountainDetail/CourseItem.tsx
+++ frontend/mountaindo/src/components/mountainDetail/CourseItem.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import {View, Text, StyleSheet, Image, Pressable} from 'react-native';
+import {View, StyleSheet, Image, Pressable} from 'react-native';
+import AppText from '../AppText';
 
 interface Props {
   id: number;
@@ -33,10 +34,12 @@ function CourseItem({
             imageSrc,
           );
         }}>
-        <Text style={styles.trailText}>{trail}</Text>
+        <AppText style={styles.trailText}>{trail}</AppText>
         <Image source={imageSrc} style={styles.imageSrc} />
-        <Text style={styles.levelText}>난이도: {level}</Text>
-        <Text style={styles.timeDurationText}>이동 시간: {timeDuration}</Text>
+        <AppText style={styles.levelText}>난이도: {level}</AppText>
+        <AppText style={styles.timeDurationText}>
+          이동 시간: {timeDuration}
+        </AppText>
       </Pressable>
     </View>
   );
@@ -50,7 +53,6 @@ const styles = StyleSheet.create({
   },
   trailText: {
     fontSize: 12,
-    color: 'black',
   },
   imageSrc: {
     width: 90,
@@ -58,11 +60,9 @@ const styles = StyleSheet.create({
   },
   levelText: {
     fontSize: 12,
-    color: 'black',
   },
   timeDurationText: {
     fontSize: 10,
-    color: 'black',
   },
 });
 
