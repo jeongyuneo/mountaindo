59caf86e9ae01a305c9ec1d9fad007e7dd3a0f98
giyoon27 <giyoon27@naver.com>
2022-09-29 10:59:01 +0900

    feat: SaveSurveyRequest 추가

---
+++ backend/src/main/java/com/hanssarang/backend/survey/controller/dto/SaveSurveyRequest.java
@@ -0,0 +1,15 @@
+package com.hanssarang.backend.survey.controller.dto;
+
+import lombok.*;
+
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class SaveSurveyRequest {
+
+    private int level;
+    private int preferredMountainLocation;
+    private int preferredClimbingStyle;
+    private int preferredHikingTime;
+}
