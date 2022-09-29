b607c83cdc9cdb4c7e9e0d9cb64d6354c1776358
giyoon27 <giyoon27@naver.com>
2022-09-29 10:58:39 +0900

    feat: SurveyService - 설문조사 응답 결과 저장 추가

---
+++ backend/src/main/java/com/hanssarang/backend/survey/service/SurveyService.java
@@ -0,0 +1,33 @@
+package com.hanssarang.backend.survey.service;
+
+import com.hanssarang.backend.common.exception.NotFoundException;
+import com.hanssarang.backend.member.domain.Member;
+import com.hanssarang.backend.member.domain.MemberRepository;
+import com.hanssarang.backend.survey.controller.dto.SaveSurveyRequest;
+import com.hanssarang.backend.survey.domain.Survey;
+import com.hanssarang.backend.survey.domain.SurveyRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import static com.hanssarang.backend.common.domain.ErrorMessage.NOT_FOUND_MEMBER;
+
+@RequiredArgsConstructor
+@Service
+public class SurveyService {
+
+    private final SurveyRepository surveyRepository;
+    private final MemberRepository memberRepository;
+
+    public void saveSurvey(int memberId, SaveSurveyRequest saveSurveyRequest) {
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new NotFoundException(NOT_FOUND_MEMBER));
+        Survey survey = Survey.builder()
+                .level(saveSurveyRequest.getLevel())
+                .preferredMountainLocation(saveSurveyRequest.getPreferredMountainLocation())
+                .preferredClimbingStyle(saveSurveyRequest.getPreferredClimbingStyle())
+                .preferredHikingTime(saveSurveyRequest.getPreferredHikingTime())
+                .member(member)
+                .build();
+        surveyRepository.save(survey);
+    }
+}
