abcf152b6122d6750b7e082f1d9b0e97dee73c3f
DESKTOP-KVCQHCD\SSAFY <pbsu1213@hanmail.net>
2022-09-29 16:45:27 +0900

    feat: MountainRepository - isHot 쿼리문 수정

--- backend/src/main/java/com/hanssarang/backend/mountain/domain/MountainRepository.java
+++ backend/src/main/java/com/hanssarang/backend/mountain/domain/MountainRepository.java
@@ -23,13 +23,14 @@ public interface MountainRepository extends JpaRepository<Mountain, Integer> {
 
     @Query(value = "select m.* " +
             "from mountain m " +
-            "inner join (select h.trail_id, count(h.trail_id) count, t.mountain_id " +
+            "left join (select h.trail_id, count(h.trail_id) count, t.mountain_id " +
             "            from hiking h " +
             "            inner join trail t " +
             "            on t.trail_id = h.trail_id " +
-            "            group by h.trail_id) v " +
+            "            group by t.trail_id) v " +
             "on m.mountain_id = v.mountain_id " +
             "group by m.mountain_id " +
+            "having sum(v.count) " +
             "LIMIT 10", nativeQuery = true)
     List<Mountain> findIsHot();
 
