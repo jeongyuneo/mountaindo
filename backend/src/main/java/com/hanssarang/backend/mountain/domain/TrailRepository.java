5c31f1234e9dea5966965a9f81e4e67979150a0f
DESKTOP-KVCQHCD\SSAFY <pbsu1213@hanmail.net>
2022-09-29 16:35:22 +0900

    refactor: TrailRepository - package 위치 변경

--- backend/src/main/java/com/hanssarang/backend/mountain/domain/TrailRepository.java
+++ backend/src/main/java/com/hanssarang/backend/mountain/domain/TrailRepository.java
@@ -1,6 +1,5 @@
-package com.hanssarang.backend.mountain.repository;
+package com.hanssarang.backend.mountain.domain;
 
-import com.hanssarang.backend.mountain.domain.Trail;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TrailRepository extends JpaRepository<Trail, Integer> {
