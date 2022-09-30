0c795d0d234479c4c58c8b6447cd5d34aa1d6989
jeonyuneo <piq2255@gmail.com>
2022-09-30 09:31:38 +0900

    feat: SignUpRequest - 엔터티 변환 메소드 추가

--- backend/src/main/java/com/hanssarang/backend/member/controller/dto/SignUpRequest.java
+++ backend/src/main/java/com/hanssarang/backend/member/controller/dto/SignUpRequest.java
@@ -1,7 +1,9 @@
 package com.hanssarang.backend.member.controller.dto;
 
 import com.hanssarang.backend.common.domain.Address;
+import com.hanssarang.backend.member.domain.Member;
 import lombok.*;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.time.LocalDate;
 
@@ -18,4 +20,21 @@ public class SignUpRequest {
     private String phone;
     private Address address;
     private String nickname;
+
+    public Member toEntity(PasswordEncoder passwordEncoder) {
+        return Member.builder()
+                .email(email)
+                .password(passwordEncoder.encode(password))
+                .name(name)
+                .birth(birth)
+                .phone(phone)
+                .nickname(nickname)
+                .address(Address.builder()
+                        .si(address.getSi())
+                        .gu(address.getGu())
+                        .dong(address.getDong())
+                        .build())
+                .isActive(true)
+                .build();
+    }
 }
