0d5aeaa1dfdcd2dcf33b8d6a593084bd611877e9
giyoon27 <giyoon27@naver.com>
2022-09-30 22:16:34 +0900

    refactor: 불필요한 메서드 삭제

--- backend/src/main/java/com/hanssarang/backend/hiking/domain/HikingRepository.java
+++ backend/src/main/java/com/hanssarang/backend/hiking/domain/HikingRepository.java
@@ -5,6 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface HikingRepository extends JpaRepository<Hiking, Integer> {
-
-    Optional<Hiking> findByMemberIdAndId(int memberId, int hikingId);
 }
