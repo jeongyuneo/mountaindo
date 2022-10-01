cee3e9260ed83dc2212ace7731b8a05249b7558a
DESKTOP-KVCQHCD\SSAFY <pbsu1213@hanmail.net>
2022-10-01 18:17:44 +0900

    feat: ErrorMessage - 에러 메세지 추가

--- backend/src/main/java/com/hanssarang/backend/common/domain/ErrorMessage.java
+++ backend/src/main/java/com/hanssarang/backend/common/domain/ErrorMessage.java
@@ -11,6 +11,7 @@ public class ErrorMessage {
     public static final String FAIL_TO_DELETE_MEMBER = "회원탈퇴에 실패하였습니다.";
     public static final String FAIL_TO_CHECK_NICKNAME = "중복된 닉네임이 있습니다.";
     public static final String FAIL_TO_LOGIN = "로그인에 실패하였습니다.";
+    public static final String FAIL_TO_SEND_PASSWORD = "임시 비밀번호 전송에 실패하였습니다.";
 
     public static final String NOT_FOUND_MEMBER = "사용자를 찾을 수 없습니다.";
     public static final String NOT_FOUND_HIKING = "등산 정보를 찾을 수 없습니다.";
@@ -24,4 +25,6 @@ public class ErrorMessage {
     public static final String FAIL_TO_CREATE_HIKING = "등산 정보 저장을 실패했습니다.";
 
     public static final String WRONG_PATH = "잘못된 경로입니다.";
+
+    public static final String TOKEN_NOT_EQUAL = "이메일 인증번호가 일치하지 않습니다.";
 }
