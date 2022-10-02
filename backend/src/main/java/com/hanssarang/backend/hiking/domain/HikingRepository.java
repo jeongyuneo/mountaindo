d9ba8273817e6f90f243d56ce47e023e7ff458d8
giyoon27 <giyoon27@naver.com>
2022-10-02 17:45:43 +0900

    fix: 등산 기록 조회 쿼리 추가

--- backend/src/main/java/com/hanssarang/backend/hiking/domain/HikingRepository.java
+++ backend/src/main/java/com/hanssarang/backend/hiking/domain/HikingRepository.java
@@ -1,8 +1,16 @@
 package com.hanssarang.backend.hiking.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-import java.util.Optional;
+import java.util.List;
 
 public interface HikingRepository extends JpaRepository<Hiking, Integer> {
+
+    @Query(value = "select * " +
+            "from hiking " +
+            "where member_id = :memberId " +
+            "and created_date in (select max(created_date) from hiking group by trail_id) " +
+            "order by created_date desc", nativeQuery = true)
+    List<Hiking> findAllHikings(int memberId);
 }
