9835c0b7de8203760db46c3b924aaeddf369198b
DESKTOP-KVCQHCD\SSAFY <pbsu1213@hanmail.net>
2022-10-02 20:04:45 +0900

    feat: HikingRepository - 하이킹 값 가져오는 기능 추가

--- backend/src/main/java/com/hanssarang/backend/hiking/domain/HikingRepository.java
+++ backend/src/main/java/com/hanssarang/backend/hiking/domain/HikingRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface HikingRepository extends JpaRepository<Hiking, Integer> {
+    Optional<Hiking> findByIdAndMemberId(int hikingId, int memberId);
 }
