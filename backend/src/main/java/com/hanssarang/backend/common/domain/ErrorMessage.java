35a055bb79a44a15c542f710692037e5a0607d79
jeonyuneo <piq2255@gmail.com>
2022-10-02 22:07:35 +0900

    style: ErrorMessage - 등산 정보 저장 에러 메시지 오타 수정

--- backend/src/main/java/com/hanssarang/backend/common/domain/ErrorMessage.java
+++ backend/src/main/java/com/hanssarang/backend/common/domain/ErrorMessage.java
@@ -12,7 +12,12 @@ public class ErrorMessage {
     public static final String FAIL_TO_SEARCH_MOUNTAIN = "산 검색에 실패했습니다.";
     public static final String FAIL_TO_SEARCH_TRAIL = "등산로 검색에 실패했습니다.";
     public static final String FAIL_TO_SEARCH_MOUNTAIN_OR_TRAIL = "산 또는 등산로 검색에 실패했습니다.";
-    public static final String FAIL_TO_CREATE_HIKING = "등산 정보 저장애 실패했습니다.";
+    public static final String FAIL_TO_CREATE_HIKING = "등산 정보 저장에 실패했습니다.";
+
+    public static final String NOT_FOUND_MEMBER = "사용자를 찾을 수 없습니다.";
+    public static final String NOT_FOUND_HIKING = "등산 정보를 찾을 수 없습니다.";
+    public static final String NOT_FOUND_MOUNTAIN = "해당 산을 찾을 수 없습니다.";
+    public static final String NOT_FOUND_TRAIL = "해당 등산로를 찾을 수 없습니다.";
 
     public static final String NOT_EQUAL_PASSWORD = "비밀번호가 일치하지 않습니다.";
     public static final String NOT_FOUND_MEMBER = "사용자를 찾을 수 없습니다.";
