2552a2108e3a2ce44f43105aa92ea550b3ea4b2c
jeonyuneo <piq2255@gmail.com>
2022-10-03 16:20:06 +0900

    feat: MemberBasedRecommendation 추가

---
+++ backend/src/main/java/com/hanssarang/backend/member/domain/MemberBasedRecommendation.java
@@ -0,0 +1,29 @@
+package com.hanssarang.backend.member.domain;
+
+import com.hanssarang.backend.common.domain.BaseEntity;
+import com.hanssarang.backend.mountain.domain.Trail;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@SuperBuilder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AttributeOverride(name = "id", column = @Column(name = "member_based_recommendation_id"))
+@Entity
+public class MemberBasedRecommendation extends BaseEntity {
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "trail_id")
+    private Trail trail;
+}
