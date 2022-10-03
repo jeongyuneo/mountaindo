69090445d3c80ba989e41a77c62251925637b369
DESKTOP-KVCQHCD\SSAFY <pbsu1213@hanmail.net>
2022-10-03 17:43:50 +0900

    refactor: SendVerificationNumberRequest - dto 명 수정

--- backend/src/main/java/com/hanssarang/backend/member/controller/dto/SendVerificationNumberRequest.java
+++ backend/src/main/java/com/hanssarang/backend/member/controller/dto/SendVerificationNumberRequest.java
@@ -6,7 +6,7 @@ import lombok.*;
 @Builder
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class EmailValidationRequest {
+public class SendVerificationNumberRequest {
 
     private String email;
 }
