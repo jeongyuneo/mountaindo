76bb6308794bb29dbeab7766b8f91a0faeb59fb7
DESKTOP-KVCQHCD\SSAFY <pbsu1213@hanmail.net>
2022-10-04 22:58:48 +0900

    feat: WebConfig - 인터셉터 추가

--- backend/src/main/java/com/hanssarang/backend/config/WebConfig.java
+++ backend/src/main/java/com/hanssarang/backend/config/WebConfig.java
@@ -1,13 +1,17 @@
 package com.hanssarang.backend.config;
 
+import com.hanssarang.backend.interceptor.JwtAuthInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import static com.hanssarang.backend.common.domain.Message.AUTHORIZATION;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    private static final String EXCLUDE_PATH = "/api/v1/members/**";
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -17,5 +21,11 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedHeaders("*")
                 .exposedHeaders(AUTHORIZATION)
                 .maxAge(6000);
+    }
+
+    @Override
+    public void addInterceptors(final InterceptorRegistry interceptorRegistry) {
+        interceptorRegistry.addInterceptor(new JwtAuthInterceptor())
+                .excludePathPatterns(EXCLUDE_PATH);
     }
 }
