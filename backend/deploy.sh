1418b48cc5124edaaf6698f5d4228b04ba79d459
jeonyuneo <piq2255@gmail.com>
2022-10-04 22:01:37 +0900

    feat: 도커 이미지 백업 명령어 추가

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -2,6 +2,8 @@
 
 ./gradlew build
 
+sudo docker cp mountaindo_deploy:/home/img/. ~
+
 sudo docker stop mountaindo_deploy
 
 sudo docker rm mountaindo_deploy
@@ -11,5 +13,7 @@ sudo docker rmi mountaindo_deploy
 sudo docker build -t mountaindo_deploy .
 
 sudo docker run -d -p 8080:8080 --name mountaindo_deploy mountaindo_deploy
+
+sudo docker cp ~/img/. mountaindo_deploy:/home
 
 sudo docker logs -f mountaindo_deploy
