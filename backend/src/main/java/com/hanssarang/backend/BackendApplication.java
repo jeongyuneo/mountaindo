9e8af21898cedc48d313f4a934fec27a8298ce57
giyoon27 <giyoon27@naver.com>
2022-10-05 10:53:39 +0900

    refactor: 불필요한 어노테이션 삭제

--- backend/src/main/java/com/hanssarang/backend/BackendApplication.java
+++ backend/src/main/java/com/hanssarang/backend/BackendApplication.java
@@ -2,10 +2,8 @@ package com.hanssarang.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
-@EnableAspectJAutoProxy
 public class BackendApplication {
 
     public static void main(String[] args) {
