f92920532f583f1b6a00917ba7faed814f28f31e
giyoon27 <giyoon27@naver.com>
2022-10-05 10:10:54 +0900

    feat: 로깅 적용

--- backend/src/main/java/com/hanssarang/backend/BackendApplication.java
+++ backend/src/main/java/com/hanssarang/backend/BackendApplication.java
@@ -2,8 +2,10 @@ package com.hanssarang.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class BackendApplication {
 
     public static void main(String[] args) {
---
+++ backend/src/main/java/com/hanssarang/backend/aop/LoggingAspect.java
@@ -0,0 +1,22 @@
+package com.hanssarang.backend.aop;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Aspect
+@Component
+public class LoggingAspect {
+
+    private static Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
+
+    @Before(value = "execution(* com.hanssarang..controller.*Controller.*(..)) || execution(* com.hanssarang..service.*Service.*(..))")
+    public void logging(JoinPoint joinPoint) {
+        logger.debug("메소드 선언부 : {}, 전달 파라미터 : {}", joinPoint.getSignature(), Arrays.toString(joinPoint.getArgs()));
+    }
+}
