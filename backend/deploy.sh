5f9e133b2908b16c5ffd7b3b5b594253da5d6711
jeonyuneo <piq2255@gmail.com>
2022-10-06 14:02:10 +0900

    release: deploy.sh 수정

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -2,8 +2,6 @@
 
 ./gradlew build
 
-sudo docker cp mountaindo_deploy:/home/. ~/img
-
 sudo docker stop mountaindo_deploy
 
 sudo docker rm mountaindo_deploy
@@ -13,7 +11,5 @@ sudo docker rmi mountaindo_deploy
 sudo docker build -t mountaindo_deploy .
 
 sudo docker run -d -p 8080:8080 --name mountaindo_deploy mountaindo_deploy
-
-sudo docker cp ~/img/. mountaindo_deploy:/home
 
 sudo docker logs -f mountaindo_deploy
