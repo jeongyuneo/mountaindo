1dd08688898677ba98c561a8aa2a981e57b176d5
jeonyuneo <piq2255@gmail.com>
2022-10-07 06:15:13 +0900

    release: deploy.sh 볼륨 설정 추가 및 네이밍 수정

--- backend/spring-boot/deploy.sh
+++ backend/spring-boot/deploy.sh
@@ -2,14 +2,14 @@
 
 ./gradlew build
 
-sudo docker stop mountaindo_deploy
+sudo docker stop mountaindo_deploy_spring
 
-sudo docker rm mountaindo_deploy
+sudo docker rm mountaindo_deploy_spring
 
-sudo docker rmi mountaindo_deploy
+sudo docker rmi mountaindo_deploy_spring
 
-sudo docker build -t mountaindo_deploy .
+sudo docker build -t mountaindo_deploy_spring .
 
-sudo docker run -d -p 8080:8080 --name mountaindo_deploy mountaindo_deploy
+sudo docker run -d -p 8080:8080 --name mountaindo_deploy_spring -v /home/ubuntu/img:/home/ mountaindo_deploy_spring
 
-sudo docker logs -f mountaindo_deploy
+sudo docker logs -f mountaindo_deploy_spring
